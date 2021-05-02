6a05cd3eebc569dd774f768a2f19f03be8e653f4
Kevin Cenerino <cenerinok@gmail.com>
2021-05-02 14:21:56 +0200

    Add index.php

---
+++ Eval1/index.php
@@ -0,0 +1,75 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Association des photographes, passioné de photograpie , amateur ou profesionel, partager vos photos et réalisation">
+    <title>Association des photographes</title>
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Asso-photographes</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" href="index.php">Acceuil</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="nos-activites.html">Nos événements</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="nous-rejoindre.html">Nous rejoindre</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.html">Nous contacter</a>
+            </ul>
+          </div>
+        </div>
+      </nav>
+     
+        <div class="img_header">
+        <h1>Passioné(e) de photographie ?</h1>
+      </div>
+      </header>
+
+    <main class="main">
+  
+      <section class="main_left">
+        <h2>L'association : qui somme nous ?</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur tempore, fugit hic perspiciatis unde illo? Eius aut harum deserunt tenetur id voluptatum ipsa explicabo. Veniam iure mollitia consequatur neque id labore beatae! Nulla ratione, sequi animi itaque non ipsum iste illo sunt quis rem fugit, tempore asperiores libero? Earum quam fuga dolor sequi dignissimos, et est accusamus modi quis quia blanditiis ex eaque repellendus unde enim ipsam iusto libero reprehenderit delectus vel error Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus numquam, error, quisquam soluta reprehenderit, cum similique itaque assumenda dolorum quasi a laboriosam alias magni possimus mollitia repellendus asperiores vitae sapiente?</p>
+        <img src="/media/mainfirst.jpg" alt="Photo d'une fleur">
+      </section>
+
+
+      <section class="main_right">
+        <h2>L'art de la photographie</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur tempore, fugit hic perspiciatis unde illo? Eius aut harum deserunt tenetur id voluptatum ipsa explicabo. Veniam iure mollitia consequatur neque id labore beatae! Nulla ratione, sequi animi itaque non ipsum iste illo sunt quis rem fugit, tempore asperiores libero? Earum quam fuga dolor sequi dignissimos, et est accusamus modi quis quia blanditiis ex eaque repellendus unde enim ipsam iusto libero reprehenderit delectus vel error Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus numquam, error, quisquam soluta reprehenderit, cum similique itaque assumenda dolorum quasi a laboriosam alias magni possimus mollitia repellendus asperiores vitae sapiente?</p>
+        <img src="/media/mainsecond.jpg" alt="Photo d'une fleur">
+      </section>
+
+      <h3>La beauté de notre passion</h3>
+     <article class="main_bottom">
+          <img src="/media/fleur.jpg" alt="Photo d'une fleur">
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos laudantium aperiam id eligendi repellendus maiores cupiditate dolorem, esse sunt facere nostrum harum magni a reprehenderit quibusdam inventore deleniti dolore libero quis explicabo repellat deserunt magnam. Magni quam soluta aliquid. Similique! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi possimus nisi adipisci, consequatur aspernatur sapiente tempora nemo illo minima molestias Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus eius, et exercitationem numquam pariatur fugiat dolorem tempora labore nam voluptatem veritatis temporibus voluptas harum maxime nihil placeat at facere! Tempore!.</p>
+          <img src="/media/forest.jpg" alt="Photo d'une forêt">
+     </article>
+
+    </main>
+
+    <footer>
+      <div class="line"></div>
+      <p> A tous les photographes amateurs ou professionels, tous droit libres, Evaluation Studi 2021</p>
+      </footer>
+
+    <script src="bootstrap.min.js"></script>
+</body>
+</html>
